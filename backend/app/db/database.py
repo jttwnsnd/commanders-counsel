1c96810652763b4977f3d88487a9e7c43239c2de
jttownsend <jt.a.townsend@gmail.com>
2026-04-07 22:22:42 -0400

    turn off echo, confirmed it works

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 from app.core.config import settings
 
-engine = create_async_engine(settings.DATABASE_URL, echo=True)
+engine = create_async_engine(settings.DATABASE_URL, echo=False)
 
 AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
